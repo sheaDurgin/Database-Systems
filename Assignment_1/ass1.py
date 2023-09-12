3b8ffe0b01c8a35a1d09f70fe79261fbf78d818e
sheaDurgin <shea.durgin@maine.edu>
2023-09-12 14:35:14 -0400

    uhh how does this work?

--- Assignment_1/ass1.py
+++ Assignment_1/ass1.py
@@ -23,8 +23,10 @@ def extract_keywords(keywords_element):
         pattern = re.compile(r'\b(key ?-?words)(.*)\s*:\s*')
         if re.search(pattern, keywords_text.lower()):
             keywords_text = re.sub(pattern, "", keywords_text.lower())
-        # if "keywords:" in keywords_text.lower():
-        #     keywords_text = keywords_text.lower().replace("keywords:", "")
+        if "keywords:" in keywords_text.lower():
+            keywords_text = keywords_text.lower().replace("keywords:", "")
+        if "index terms:" in keywords_text.lower():
+            keywords_text = keywords_text.lower().replace("keywords:", "")
         keywords = [keyword.strip() for keyword in keywords_text.split(',')]
     return keywords
 
@@ -34,11 +36,23 @@ def remove_superscripts(soup):
         element.extract()
 
 # folder_path = '../arxiv-papers/'
-folder_path = '../../../../shea.durgin/Database-Systems/arxiv-papers/'
+#folder_path = '../../../../shea.durgin/Database-Systems/arxiv-papers/'
+folder_path = '../arxiv-papers/'
 # chane to within Ass1 folder
-output_tsv_path = '../output_testing.tsv'
+output_tsv_path = '../output_testing5.tsv'
 
 html_files = sorted([filename for filename in os.listdir(folder_path) if filename.endswith('.html')])
+
+def scrape_emails_from_html(soup):
+    # Define a regular expression pattern to match email addresses
+    email_pattern = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
+
+    # Find all email addresses in the HTML using the re.findall method
+    emails = re.findall(email_pattern, str(soup))
+
+    unique_emails = list(set(emails))
+
+    return unique_emails
 
 def process_html_file(filename):
     if filename.endswith('.html'):
@@ -52,13 +66,14 @@ def process_html_file(filename):
             remove_superscripts(soup)
             author_names = [extract_text_from_spans(author, 'ltx_personname', 'Author Names') for author in authors]
             affiliations = [extract_text_from_spans(author, 'ltx_role_address', 'Affiliations') for author in authors]
-            emails = [extract_text_from_spans(author, 'ltx_role_email', 'Emails') for author in authors]
+            #emails = [extract_text_from_spans(author, 'ltx_role_email', 'Emails') for author in authors]
+            emails = scrape_emails_from_html(soup)
 
             abstract_element = soup.find('p', class_='ltx_p')
             abstract = clean_text(abstract_element.get_text(strip=True)) if abstract_element else ""
 
             pattern = re.compile(r'.*key ?-?words.*', re.IGNORECASE)
-            keywords_element = soup.find('div', class_="ltx_keywords") or soup.find('div', class_='ltx_classification') or soup.find(['div', 'span'], text=pattern)
+            keywords_element = soup.find('div', class_="ltx_keywords") or soup.find('div', class_='ltx_classification') or soup.find(['div', 'span'], string=pattern)
             # keywords_element = soup.find(['div', 'span'], text=pattern)
             keywords = extract_keywords(keywords_element)
 
